cb2362285a24ed88c4684fe13586b1627079e4a2
Tarun Kumar Sukhu <tksukhu@gmail.com>
2021-07-01 12:26:50 +0530

    updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { useDeno } from 'framework/react'
 import React from 'react'
 import Logo from '~/components/logo.tsx'
 import useCounter from '~/lib/useCounter.ts'
+import { Thing } from 'my-deno-lib';
 
 export default function Home() {
   const [count, isSyncing, increase, decrease] = useCounter()
@@ -15,6 +16,7 @@ export default function Home() {
       </head>
       <p className="logo"><Logo /></p>
       <h1>Welcome to use <strong>Aleph.js</strong>!</h1>
+      <Thing />
       <p className="links">
         <a href="https://alephjs.org" target="_blank">Website</a>
         <span></span>
